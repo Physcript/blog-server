2cdd19e8ef848181ad500308bf3f99ee231e31f8
patrick <physcript@gmail.com>
2022-01-13 13:52:16 +0800

    fixing updated blog, creating blog

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -10,7 +10,7 @@ const router = express.Router()
 router.get('/:blogID', blogCon.readBlog)
 router.post('/create', blogCon.createBlog)
 router.post('/readAll', blogCon.readAllBlog)
-router.post('/update/:blogID', blogCon.editBlog)
+router.patch('/update/:blogID', blogCon.editBlog)
 router.delete('/:blogID', blogCon.deleteBlog)
 router.get('/',blogCon.readAllBlog)
 
